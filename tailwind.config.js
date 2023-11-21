20a6a0423a8c83264de75c20ecdd3bdee98d03a8
Eduardo Stuart <e@s.tuart.me>
2023-11-21 12:43:29 +0100

    style fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+        mono: ['"Space Mono"', ...defaultTheme.fontFamily.mono],
       },
     },
   },
